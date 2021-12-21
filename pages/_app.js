bb267cc8fb10848ccc4c74ed6fc387842450332c
cllcnkrt <cllcnkrt@outlook.com>
2021-12-21 18:56:29 +0300

    moralis added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import { MoralisProvider } from 'react-moralis';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId="v3ASJi2ZChj1PhIokW5aNdAn1Y2xcVREDrUo9eXk"
+      serverUrl="https://oyrswild9t3a.usemoralis.com:2053/server"
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
