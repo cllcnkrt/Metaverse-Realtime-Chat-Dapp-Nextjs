f8f4fdcc0020f80c77701db85c50a54946c2e8df
cllcnkrt <cllcnkrt@outlook.com>
2021-12-23 17:00:42 +0300

    real time chat active

--- components/Message.js
+++ components/Message.js
@@ -26,8 +26,13 @@ function Message({ message }) {
         <p>{message.get('message')}</p>
       </div>
 
-      
-
+      <p
+        className={`absolute -bottom-5 text-xs ${
+          isUserMessage ? 'text-pink-300' : 'text-blue-400'
+        }`}
+      >
+        {message.get('username')}
+      </p>
     </div>
   );
 }
