42205da94c7af7cd9d06dc6f1b42df5b95cd00a4
cllcnkrt <cllcnkrt@outlook.com>
2021-12-23 14:09:46 +0300

    Messages component

--- components/Messages.js
+++ components/Messages.js
@@ -1,9 +1,23 @@
+import { ByMoralis, useMoralis, useMoralisQuery } from 'react-moralis';
+
 function Messages() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const { user } = useMoralis();
+
+  return (
+    <div className="pb-56">
+      <div className="my-5">
+        <ByMoralis
+          variant="dark"
+          style={{ marginLeft: 'auto', marginRight: 'auto' }}
+        />
+      </div>
+      <div></div>
+      <div></div>
+      <div className="text-center text-gray-400 mt-5">
+        <p>You're up to date {user.getUsername()}!</p>
+      </div>
+    </div>
+  );
 }
 
-export default Messages
+export default Messages;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { useMoralis } from 'react-moralis';
 import Header from '../components/Header';
 import Login from '../components/Login';
+import Messages from '../components/Messages';
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
